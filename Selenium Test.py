132631cea6dc1e501010ea91141e1b74941100bd
Eoin Fitzsimons <eoin00fitzsimons@gmail.com>
2024-05-07 23:10:05 +0100

    Update Selenium Test.py

--- Selenium Test.py
+++ Selenium Test.py
@@ -15,7 +15,8 @@ driver = webdriver.Edge(service=webdriver_service, options=options)
 
 # Navigate to a blank page
 driver.get('about:blank')
-
+# Wait for 1 second
+time.sleep(11)
 # Navigate to the web application
 driver.get('http://localhost:3000')
 
@@ -28,11 +29,35 @@ alert = Alert(driver)
 # Enter the API key into the prompt
 alert.send_keys('9067dc23064fbdb794f79053211c2c4395c0d8bea8208194f8583ba42b8946d09acadcc4252e72bb')
 
+# Wait for 1 second
+time.sleep(1)
+
 # Accept the prompt
 alert.accept()
 
-# Wait for 10 seconds
-time.sleep(10)
+# Wait for 5 seconds
+time.sleep(2)
+
+# Refresh the page
+driver.refresh()
+
+# Wait for the prompt to appear again
+driver.implicitly_wait(10)
+
+# Switch to the alert
+alert = Alert(driver)
+
+# Enter the wrong API key into the prompt
+alert.send_keys('wrong_key')
+
+# Wait for 1 second
+time.sleep(1)
+
+# Accept the prompt
+alert.accept()
+
+# Wait for 5 seconds
+time.sleep(2)
 
 # Don't forget to quit the driver when you're done
 driver.quit()
